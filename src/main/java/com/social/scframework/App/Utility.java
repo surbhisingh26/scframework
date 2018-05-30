d289edf78806694e75b6c1effa8de33c3e035577
[paras@crucialbits.com] <surbhi.singh.ss05@gmail.com>
2018-05-30 21:01:46 +0530

    Encryption method added to utility

--- src/main/java/com/social/scframework/App/Utility.java
+++ src/main/java/com/social/scframework/App/Utility.java
@@ -2,6 +2,8 @@ package com.social.scframework.App;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -86,5 +88,24 @@ public class Utility {
 			
 		}
 		return changedData;
+	}
+	public String generateHash(String input) {
+		StringBuilder hash = new StringBuilder();
+
+		try {
+			MessageDigest sha = MessageDigest.getInstance("SHA-1");
+			byte[] hashedBytes = sha.digest(input.getBytes());
+			char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+					'a', 'b', 'c', 'd', 'e', 'f' };
+			for (int idx = 0; idx < hashedBytes.length; ++idx) {
+				byte b = hashedBytes[idx];
+				hash.append(digits[(b & 0xf0) >> 4]);
+				hash.append(digits[b & 0x0f]);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			// handle error here.
+		}
+
+		return hash.toString();
 	}
 }
